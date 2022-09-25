ed1e8a813e233a4b197530e6eb5be7484c079a35
heyitspratham <theprathamshukla@gmail.com>
2022-09-25 22:02:45 +0530

    small changes

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,7 +2,7 @@ import "./contact.scss"
 
 const Contact = () => {
   return (
-    <div className="contact"></div>
+    <div className="contact" id="contact">This is for contacts</div>
   )
 }
 
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,7 +1,26 @@
 import "./topbar.scss"
+import {Person, Mail} from "@material-ui/icons";
+
 
 export default function Topbar() {
   return (
-    <div className="topbar"></div>
+    <div className="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo" >genius.</a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>+91 8979072845</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>theprathamshukla@gmail.com</span>
+          </div>
+        </div>
+        <div className="right">
+          Right
+        </div>
+      </div>
+    </div>
   )
 }
