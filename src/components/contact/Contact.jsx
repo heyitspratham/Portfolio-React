43f6bdfa68f00aa00e79f770a9ae3ee7d84e9a5f
heyitspratham <theprathamshukla@gmail.com>
2022-09-26 23:09:16 +0530

    t

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,7 +2,7 @@ import "./contact.scss"
 
 const Contact = () => {
   return (
-    <div className="contact" id="contact">This is for contacts</div>
+    <div className="contact" id="contact">This is for contact</div>
   )
 }
 
