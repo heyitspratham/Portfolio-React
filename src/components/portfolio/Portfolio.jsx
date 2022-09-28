4a5cbf67984176a5bb607913b14d15af25adb7cb
heyitspratham <theprathamshukla@gmail.com>
2022-09-28 23:06:52 +0530

    intro done

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,6 +2,21 @@ import "./portfolio.scss"
 
 export default function Portfolio() {
   return (
-    <div className="portfolio" id="portfolio"></div>
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1> 
+      <ul>
+        <li>Featured</li>
+        <li>web App</li>
+        <li>Mobile App</li>
+        <li>Designing</li>
+        <li>Branding</li>
+      </ul>
+
+      <div className="container">
+        <div className="item">
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
