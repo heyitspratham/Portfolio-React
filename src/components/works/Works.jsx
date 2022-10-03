b58ca192e8fa69683cf063e03d801ff9f771ae14
heyitspratham <theprathamshukla@gmail.com>
2022-10-03 17:43:32 +0530

    work

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -2,6 +2,15 @@ import "./works.scss"
 
 export default function Works() {
   return (
-    <div className="works" id="works"></div>
+    <div className="works" id="works">
+      <div className="slider">
+        <div className="container">
+          <div className="item">
+            <div className="left"></div>
+            <div className="right"></div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
